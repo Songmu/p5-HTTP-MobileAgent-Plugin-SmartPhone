895176ba2d05e73252b4954601a6b57befdb7266
Masayuki Matsuki <y.songmu@gmail.com>
2011-12-18 13:32:12 +0900

    initial commit

---
+++ lib/HTTP/MobileAgent/Plugin/SmartPhone.pm
@@ -0,0 +1,32 @@
+package HTTP::MobileAgent::Plugin::SmartPhone;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTTP::MobileAgent::Plugin::SmartPhone -
+
+=head1 SYNOPSIS
+
+  use HTTP::MobileAgent::Plugin::SmartPhone;
+
+=head1 DESCRIPTION
+
+HTTP::MobileAgent::Plugin::SmartPhone is
+
+=head1 AUTHOR
+
+Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
