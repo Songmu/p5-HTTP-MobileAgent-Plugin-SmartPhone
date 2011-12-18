e622f4fabfc412cc7b2521a2d468657ae40fc988
Masayuki Matsuki <y.songmu@gmail.com>
2011-12-18 15:54:02 +0900

    rough implement.

--- lib/HTTP/MobileAgent/Plugin/SmartPhone.pm
+++ lib/HTTP/MobileAgent/Plugin/SmartPhone.pm
@@ -1,7 +1,48 @@
 package HTTP::MobileAgent::Plugin::SmartPhone;
 use strict;
 use warnings;
+use utf8;
 our $VERSION = '0.01';
+
+sub HTTP::MobileAgent::is_smartphone {
+    my $self = shift;
+    $self->is_ios || $self->is_android;
+}
+
+sub HTTP::MobileAgent::is_ios {
+    my $self = shift;
+    $self->user_agent =~ /iP(?:one|od|ad)/;
+}
+
+sub HTTP::MobileAgent::is_iphone {
+    shift->user_agent =~ /iPhone/;
+}
+
+sub HTTP::MobileAgent::is_ipad {
+    shift->user_agent =~ /iPad/;
+}
+
+sub HTTP::MobileAgent::is_android {
+    shift->user_agent =~ /Android/;
+}
+
+sub HTTP::MobileAgent::ios_full_version {
+    my $self = shift;
+    return () unless $self->is_ios;
+
+    local $1;
+    my $full_version;
+    if ($self->user_agent =~ /CPU (?:iPhone )?(?:OS ((?:\d+)(?:_\d+)*) )?like/) {
+        $full_version = $1 || 1;
+    }
+    $full_version;
+}
+
+sub HTTP::MobileAgent::ios_version {
+    my ($version) = (shift->ios_full_version || '') =~ /^(\d+)/;
+    $version;
+}
+
 
 1;
 __END__
